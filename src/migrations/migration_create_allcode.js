e1c884a69f11ee9a94a56495a818fc9d70ecd831
B2103505 <phamvulamvip@gmail.com>
2025-01-21 21:02:48 +0700

    database Basic 4 bkcarebe

---
+++ src/migrations/migration_create_allcode.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('Allcodes', {
+            Id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            key: {
+                type: Sequelize.STRING
+            },
+            type: {
+                type: Sequelize.STRING
+            },
+            valueEn: {
+                type: Sequelize.STRING
+            },
+            valueVi: {
+                type: Sequelize.STRING
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+            }
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('Allcodes');
+    }
+};
---
+++ src/migrations/migration_create_booking.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('Bookings', {
+            Id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            statusId: {
+                type: Sequelize.STRING
+            },
+            doctorId: {
+                type: Sequelize.INTEGER
+            },
+            patientId: {
+                type: Sequelize.INTEGER
+            },
+            date: {
+                type: Sequelize.DATE
+            },
+            timeType: {
+                type: Sequelize.STRING
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+            }
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('Bookings');
+    }
+};
