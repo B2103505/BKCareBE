799a417612db7ec69e649292b8832147f580eec7
B2103505 <phamvulamvip@gmail.com>
2025-04-08 09:59:54 +0700

    model doctor-info

---
+++ src/migrations/migration_create_doctor_info.js
@@ -0,0 +1,58 @@
+'use strict';
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('doctor_info', {
+            Id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            doctorId: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+            },
+            priceId: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            provinceId: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            paymentId: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            addressClinic: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            nameClinic: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            note: {
+                type: Sequelize.STRING
+            },
+            count: {
+                type: Sequelize.INTEGER,
+                allowNull: true,
+                defaultValue: 0
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+            }
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('doctor_info');
+    }
+};
