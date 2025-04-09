1970a41b72fecd6a4d806fff628e3868b4f2a579
Cirioto <146433860+Cirioto@users.noreply.github.com>
2025-04-09 21:42:44 +0700

    Thêm Data Clinic thành công vào DB

--- src/route/web.js
+++ src/route/web.js
@@ -1,46 +1,49 @@
 import express from "express";
 import HomeController from "../controllers/HomeController";
 import UserController from "../controllers/UserController";
-import DoctorController from "../controllers/DoctorController"
-import PatientController from '../controllers/PatientController'
-import SpecialtyController from '../controllers/SpecialtyController'
+import DoctorController from "../controllers/DoctorController";
+import PatientController from "../controllers/PatientController";
+import SpecialtyController from "../controllers/SpecialtyController";
+import ClinicController from "../controllers/ClinicController";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get('/', HomeController.getHomePage);
-    router.get('/about', HomeController.getAbout);
+  router.get("/", HomeController.getHomePage);
+  router.get("/about", HomeController.getAbout);
 
-    router.get('/crud', HomeController.getCrud);
-    router.post('/post-crud', HomeController.postCrud);
-    router.get('/get-crud', HomeController.displayGetCrud);
+  router.get("/crud", HomeController.getCrud);
+  router.post("/post-crud", HomeController.postCrud);
+  router.get("/get-crud", HomeController.displayGetCrud);
 
-    router.get('/edit-crud', HomeController.getEditCRUD);
-    router.post('/put-crud', HomeController.putCRUD);
-    router.get('/delete-crud', HomeController.deleteCRUD);
+  router.get("/edit-crud", HomeController.getEditCRUD);
+  router.post("/put-crud", HomeController.putCRUD);
+  router.get("/delete-crud", HomeController.deleteCRUD);
 
-    //RestApi
-    router.post('/api/login', UserController.handleLogin);
-    router.get('/api/get-all-user', UserController.handleGetAllUsers);
-    router.post('/api/create-new-user', UserController.handleCreateNewUser);
-    router.put('/api/edit-user', UserController.handleEditUser);
-    router.delete('/api/delete-user', UserController.handleDeleteUser);
-    router.get('/api/allcode', UserController.getAllCode);
+  //RestApi
+  router.post("/api/login", UserController.handleLogin);
+  router.get("/api/get-all-user", UserController.handleGetAllUsers);
+  router.post("/api/create-new-user", UserController.handleCreateNewUser);
+  router.put("/api/edit-user", UserController.handleEditUser);
+  router.delete("/api/delete-user", UserController.handleDeleteUser);
+  router.get("/api/allcode", UserController.getAllCode);
 
-    router.get('/api/top-doctor-home', DoctorController.getTopDoctorHome);
-    router.get('/api/get-all-doctor', DoctorController.getAllDoctors);
-    router.post('/api/save-info-doctor', DoctorController.PostInfoDoctor);
-    router.get('/api/get-detail-doctor', DoctorController.getDetailDoctor);
-    router.post('/api/bulk-create-schedule', DoctorController.BulkCreateSchedule);
-    router.get('/api/get-schedule-doctor-bydate', DoctorController.getScheduleByDate);
-    router.get('/api/get-extra-info-doctor-byId', DoctorController.getExtraInfoById);
-    router.get('/api/get-profile-doctor-byId', DoctorController.getProfileDoctorById);
+  router.get("/api/top-doctor-home", DoctorController.getTopDoctorHome);
+  router.get("/api/get-all-doctor", DoctorController.getAllDoctors);
+  router.post("/api/save-info-doctor", DoctorController.PostInfoDoctor);
+  router.get("/api/get-detail-doctor", DoctorController.getDetailDoctor);
+  router.post("/api/bulk-create-schedule", DoctorController.BulkCreateSchedule);
+  router.get("/api/get-schedule-doctor-bydate", DoctorController.getScheduleByDate);
+  router.get("/api/get-extra-info-doctor-byId", DoctorController.getExtraInfoById);
+  router.get("/api/get-profile-doctor-byId", DoctorController.getProfileDoctorById);
 
-    router.post('/api/patient-book-appointment', PatientController.PostBookAppointment);
-    router.post('/api/verify-book-appointment', PatientController.PostVerifyBookAppointment);
+  router.post("/api/patient-book-appointment", PatientController.PostBookAppointment);
+  router.post("/api/verify-book-appointment", PatientController.PostVerifyBookAppointment);
 
-    router.post('/api/create-new-specialty', SpecialtyController.CreateNewSpecialty);
-    return app.use("/", router);
-}
+  router.post("/api/create-new-specialty", SpecialtyController.CreateNewSpecialty);
+
+  router.post("/api/create-new-clinic", ClinicController.CreateClinic);
+  return app.use("/", router);
+};
 
 module.exports = initWebRoutes;
---
+++ src/services/ClinicService.js
@@ -0,0 +1,38 @@
+import db from "../models";
+
+let CreateClinic = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (
+        !data.name ||
+        !data.address ||
+        !data.imgBase64 ||
+        !data.descriptionHTML ||
+        !data.descriptionMarkdown
+      ) {
+        resolve({
+          errCode: 1,
+          errMessage: "Missing required parameter !",
+        });
+      } else {
+        await db.Clinic.create({
+          name: data.name,
+          address: data.address,
+          image: data.imgBase64,
+          descriptionHTML: data.descriptionHTML,
+          descriptionMarkdown: data.descriptionMarkdown,
+        });
+        resolve({
+          errCode: 0,
+          errMessage: "Create specialty succeed !!!",
+        });
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+module.exports = {
+  CreateClinic: CreateClinic,
+};
