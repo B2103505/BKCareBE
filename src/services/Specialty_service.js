e4e3aee9591208c688ee9605406cdbf3d122d5d4
Cirioto <146433860+Cirioto@users.noreply.github.com>
2025-04-11 17:33:09 +0700

    Điều chỉnh lại giao diện Specialty

--- src/services/Specialty_service.js
+++ src/services/Specialty_service.js
@@ -2,105 +2,104 @@ import { where } from "sequelize";
 import db from "../models";
 
 let CreateNewSpecialtyService = (data) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            if (!data.name || !data.imgBase64 ||
-                !data.descriptionHTML || !data.descriptionMarkdown) {
-                resolve({
-                    errCode: 1,
-                    errMessage: 'Missing required parameter !'
-                })
-            } else {
-                await db.Specialty.create({
-                    name: data.name,
-                    image: data.imgBase64,
-                    descriptionHTML: data.descriptionHTML,
-                    descriptionMarkdown: data.descriptionMarkdown
-                })
-                resolve({
-                    errCode: 0,
-                    errMessage: 'Create specialty succeed !!!'
-                })
-            }
-
-        } catch (e) {
-            reject(e);
-        }
-    })
-}
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!data.name || !data.imgBase64 || !data.descriptionHTML || !data.descriptionMarkdown) {
+        resolve({
+          errCode: 1,
+          errMessage: "Missing required parameter !",
+        });
+      } else {
+        await db.Specialty.create({
+          name: data.name,
+          image: data.imgBase64,
+          descriptionHTML: data.descriptionHTML,
+          descriptionMarkdown: data.descriptionMarkdown,
+        });
+        resolve({
+          errCode: 0,
+          errMessage: "Create specialty succeed !!!",
+        });
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
 
 let getAllSpecialtyService = () => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            let data = await db.Specialty.findAll();
-            if (data && data.length > 0) {
-                data.map(item => {
-                    item.image = new Buffer(item.image, "base64").toString("binary");
-                    return item;
-                })
-            }
-            resolve({
-                errCode: 0,
-                errMessage: "Ok",
-                data
-            })
-        } catch (e) {
-            reject(e)
-        }
-    })
-}
+  return new Promise(async (resolve, reject) => {
+    try {
+      let data = await db.Specialty.findAll();
+      if (data && data.length > 0) {
+        data.map((item) => {
+          item.image = new Buffer(item.image, "base64").toString("binary");
+          return item;
+        });
+      }
+      resolve({
+        errCode: 0,
+        errMessage: "Ok",
+        data,
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
 
 let getDetailSpecialtyByIdService = (inputId, location) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            if (!inputId || !location) {
-                resolve({
-                    errCode: 1,
-                    errMessage: 'Missing parameter !'
-                })
-            } else {
-                //Tìm khoa theo id
-                let data = await db.Specialty.findOne({
-                    where: { id: inputId },
-                    attributes: ['descriptionHTML', 'descriptionMarkdown'],
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!inputId || !location) {
+        resolve({
+          errCode: 1,
+          errMessage: "Missing parameter !",
+        });
+      } else {
+        //Tìm khoa theo id
+        let data = await db.Specialty.findOne({
+          where: { id: inputId },
+          attributes: ["name", "image", "descriptionHTML", "descriptionMarkdown"],
+        });
 
-                });
+        //nếu tìm thấy chuyên khoa
+        if (data) {
+          let doctorSpecialty = [];
 
-                //nếu tìm thấy chuyên khoa
-                if (data) {
-                    let doctorSpecialty = [];
+          //nếu tìm theo location = tất cả
+          if (location === "ALL") {
+            doctorSpecialty = await db.Doctor_Info.findAll({
+              where: { SpecialtyId: inputId },
+              attributes: ["doctorId", "provinceId"],
+            });
+          } else {
+            //nếu tìm theo tỉnh thành
+            doctorSpecialty = await db.Doctor_Info.findAll({
+              where: {
+                SpecialtyId: inputId,
+                provinceId: location,
+              },
+              attributes: ["doctorId", "provinceId"],
+            });
+          }
+          data.doctorSpecialty = doctorSpecialty;
+        } else data = {};
 
-                    //nếu tìm theo location = tất cả
-                    if (location === 'ALL') {
-                        doctorSpecialty = await db.Doctor_Info.findAll({
-                            where: { SpecialtyId: inputId },
-                            attributes: ['doctorId', 'provinceId'],
-                        })
-                    } else {//nếu tìm theo tỉnh thành
-                        doctorSpecialty = await db.Doctor_Info.findAll({
-                            where: {
-                                SpecialtyId: inputId,
-                                provinceId: location
-                            },
-                            attributes: ['doctorId', 'provinceId'],
-                        })
-                    }
-                    data.doctorSpecialty = doctorSpecialty;
-                } else data = {}
-
-                resolve({
-                    errCode: 0,
-                    errMessage: 'OK',
-                    data
-                })
-            }
-        } catch (e) {
-            reject(e);
-        }
-    })
-}
+        resolve({
+          errCode: 0,
+          errMessage: "OK",
+          data,
+        });
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
 
 module.exports = {
-    CreateNewSpecialtyService, getAllSpecialtyService,
-    getDetailSpecialtyByIdService,
-}
+  CreateNewSpecialtyService,
+  getAllSpecialtyService,
+  getDetailSpecialtyByIdService,
+};
